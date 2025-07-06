d175e580ad1b5b7dea178da5bf47b320fbfb1574
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-06 15:02:17 +0530

    TIme&Space Complexity Completed

---
+++ 11_TimeAndSpace Complexity/1_TimeAndSpaceComplexity.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include<algorithm>
+// #include<climits>
+// #include<string>
+// #include<cctype>
+// #include<vector>
+// #include<set>
+// #include<iomanip>
+// #include<cmath>
+using namespace std;
+
+// Time & SPace Complexity
+int main()
+{
+    /*
+    1) What is TC. -
+    2) Notations & Bounds -
+    3) Space Complexity -
+    4)Comparision for TC's -
+    5) TC for nested Loops -
+    5.1) Basic Nested Loop QUns -
+    5.2) For Bubble Sort -
+    5.3) For Binary Search -
+    6) Recursion TC&SC -
+    6.1) For sum of N Numbers -
+    6.2) Factoerial of N - 
+    6.3) Nth Fibonacci - 
+    6.4) Merge Sort - having complexity O(n*logn) is one of the moxst optimum sorting algorithm
+    6.5) Power Funciton - 
+    6.6) Practise Quns - which is Optimized TC - 
+    7) How to Approach QUns - TC under different Limitations
+    */
+}
