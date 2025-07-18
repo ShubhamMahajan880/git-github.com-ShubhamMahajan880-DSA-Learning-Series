694433313ac37894e4132ec4a7d917b6ce0ad55b
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-18 14:51:57 +0530

    Practise Problems

--- LEarnings.cpp
+++ LEarnings.cpp
@@ -22,18 +22,85 @@ void printArray(int arr[], int n)
     cout << endl;
 }
 
-void ArrayOperation(int arr[], int n)
+void minElement(int arr[], int n)
 {
-    int sum = 0, mul = 1, avg = 0;
+    int MinElement = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        sum += arr[i];
-        mul *= arr[i];
+        if (arr[i] < MinElement)
+        {
+            MinElement = arr[i];
+        }
     }
-    avg = (sum / n);
-    cout << "Array Addition is - " << sum << endl;
-    cout << "Array Multiplication si - " << mul << endl;
-    cout << "Similarly, array avg is - " << avg << endl;
+    cout << "Hence the Min Elements is- " << MinElement << endl;
+}
+
+void maxElement(int arr[], int n)
+{
+    int MaxElement = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > MaxElement)
+        {
+            MaxElement = arr[i];
+        }
+    }
+    cout << "&the mex element is - " << MaxElement << endl;
+}
+
+int firstIndex(int arr[], int n, int x)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            cout << "So, the element foundd at - " << i << endl;
+            return 0;
+        }
+    }
+}
+
+void lastIndex(int arr[], int n, int x)
+{
+    int index = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            index = i;
+        }
+    }
+    cout << "So, the element from the last foundd at - " << index << endl;
+}
+
+void countFrequency(int arr[], int n, int x)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            count += 1;
+        }
+    }
+    cout << "Frequency of eleemnt is - " << count << " times" << endl;
+}
+
+void ArrayOpeartions(int arr[], int n)
+{
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    reverse(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
@@ -48,7 +115,17 @@ int main()
     {
         cin >> arr[i];
     }
+
+    int x;
+    cout << "mention the element you want to search for - " << endl;
+    cin >> x;
+
     printArray(arr, n);
-    ArrayOperation(arr, n);
+    minElement(arr, n);
+    maxElement(arr, n);
+    firstIndex(arr, n, x);
+    lastIndex(arr, n, x);
+    countFrequency(arr, n, x);
+    ArrayOpeartions(arr,n);
     return 0;
 }
