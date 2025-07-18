b29820652198456ff6f4123bffa48fcae0ad7d8c
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-19 00:12:46 +0530

    Practice going  on

--- LEarnings.cpp
+++ LEarnings.cpp
@@ -12,63 +12,20 @@
 // #include<iterator>
 using namespace std;
 
-void printArray(int arr[], int n)
+int stringOperation(string s)
 {
-    cout << "Entered Array we have is- " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-int majorityElement(int arr[], int n)
-{
-
-    sort(arr, arr + n);
-    cout << "Array after sorting is - " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    int expectedMajorityElement = arr[n / 2];
-    int count = 0;
-    for (int i = 0; i < n; i++)
-    {
-
-        if (arr[i] == expectedMajorityElement)
-        {
-            count++;
-        }
-    }
-    if (count > n / 2)
-    {
-        cout << "is the majority element - " << expectedMajorityElement << endl;
-    }
-    else
-    {
-        cout << "Not exist" << endl;
-    }
+    cout << "Entered String is - " << endl;
+    cout << s << endl;
     
     
 }
 
 int main()
 {
-    int n;
-    cout << "Array Size - " << endl;
-    cin >> n;
+    string s;
+    cout << "What's in your mind dude - " << endl;
+    getline(cin, s);
 
-    int arr[n];
-    cout << "Array elememts are - " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    printArray(arr, n);
-    majorityElement(arr, n);
+    stringOperation(s);
     return 0;
 }
