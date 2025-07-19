a6accea292100583ad594a66853e109c6d21f622
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-19 19:56:23 +0530

    Matrix Done in Practices

--- LEarnings.cpp
+++ LEarnings.cpp
@@ -12,7 +12,7 @@
 // #include<iterator>
 using namespace std;
 
-void printMatrix(int matrix[][3], int n, int m)
+void printMatrix(int matrix[][10], int n, int m)
 {
     cout << "Entered Matrix is - " << endl;
     for (int i = 0; i < n; i++)
@@ -25,12 +25,29 @@ void printMatrix(int matrix[][3], int n, int m)
     }
 }
 
+void BoundaryElementSUm(int matrix[][10], int n, int m)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < m; j++)
+        {
+            if (i == 0 || i == n - 1 || j == 0 || j == m - 1)
+            {
+                sum += matrix[i][j];
+            }
+        }
+    }
+    cout << "hence the sum of Boundary Elemets is - " << sum << endl;
+}
+
 int main()
 {
     int n, m;
     cout << "Size of array for row & col respectively -  - " << endl;
     cin >> n >> m;
-    int matrix[n][3];
+    int matrix[10][10];
     cout << "Print the Matrix Elements - " << endl;
     for (int i = 0; i < n; i++)
     {
@@ -40,8 +57,7 @@ int main()
         }
     }
     printMatrix(matrix, n, m);
-
-    
+    BoundaryElementSUm(matrix, n, m);
 
     return 0;
 }
