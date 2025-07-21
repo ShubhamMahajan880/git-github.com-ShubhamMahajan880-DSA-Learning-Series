6a55c15830b23227ed3b88e2a84d323106fd79b0
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-21 16:49:34 +0530

    stl done

--- LEarnings.cpp
+++ LEarnings.cpp
@@ -11,3 +11,61 @@
 // #include<list>
 // #include<iterator>
 using namespace std;
+
+int main()
+{
+    set<int> st;
+    st.insert(10);
+    st.insert(12);
+    st.insert(14);
+    st.insert(16);
+    st.insert(16);
+    st.insert(14);
+
+    for (auto i : st)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << st.size();
+    cout << endl;
+    st.erase(12);
+    for (auto i : st)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "- - - - -- - - ";
+    cout << endl;
+    int arr[10] = {1, 5, 6, 8, 1, 9, 10, 12, 47, 10};
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    set<int> st1;
+    for (int i = 0; i < 10; i++)
+    {
+        st1.insert(arr[i]);
+    }
+
+    for (auto i : st1)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    multiset<int> st2;
+    for (int i = 0; i < 10; i++)
+    {
+        st2.insert(arr[i]);
+    }
+    cout << endl;
+    for (auto i : st2)
+    {
+        cout << i << " ";
+    }
+cout << endl;           
+}
