9de03e1fe7ed9998a20e286bdba1685570d69267
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-25 21:20:17 +0530

    changes done till now

--- LEarnings.cpp
+++ LEarnings.cpp
@@ -12,55 +12,6 @@
 // #include<iterator>
 using namespace std;
 
-class College
-{
-    string studentname;
-    int rollno;
-    long double mobileno;
-
-public:
-    // Setter
-    void setName(string Name)
-    {
-        studentname = Name;
-    }
-
-    void setRollno(int Rollno)
-    {
-        rollno = Rollno;
-    }
-
-    void setMobileNo(long double MobileNo)
-    {
-        mobileno = MobileNo;
-    }
-    // Getters
-    string getName()
-    {
-        return studentname;
-    }
-
-    int getRollno()
-    {
-        return rollno;
-    }
-
-    long double getMobileNo()
-    {
-        return mobileno;
-    }
-};
-
 int main()
 {
-    College c1;
-    // Setting values throug Setters-
-    c1.setName("ShubhamMahajan");
-    c1.setMobileNo(966999880);
-    c1.setRollno(4088);
-
-    // Getting values through Getters
-    cout << c1.getName() << endl;
-    cout << c1.getMobileNo() << endl;
-    cout << c1.getRollno() << endl;
 }
--- testing.cpp
+++ testing.cpp
@@ -12,48 +12,7 @@
 // #include<iterator>
 using namespace std;
 
-class College
-{
-
-public:
-    string name;
-    string department;
-    int rollno;
-    long double contactno;
-    string *clgtype;
-
-    College(string name, string department)
-    {
-        this->name = name;
-        this->department = department;
-        clgtype = new string;
-        *clgtype = "Priovate";
-    }
-
-    // Creating Custom Copy COnstructor -
-    College(College &original)
-    {
-        cout << "Copying Original to the New COnstructor" << endl;
-        name = original.name;
-        department = original.department;
-        // clgtype = original.clgtype; // Shallow Copy change in the referencees only
-        clgtype = new string;
-        *clgtype = *original.clgtype; // Deep Copy
-    }
-
-    ~College()
-    {
-        cout << "Deletung through Destructor by defaulyt" << endl;
-    }
-};
-
 int main()
 {
-    College C1("Shubham Mahajan", "CSE AI");
 
-    cout << C1.name << endl;
-    cout << C1.department << endl;
-    cout << *C1.clgtype << endl;
-
-    return 0;
 }
