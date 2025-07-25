5259bc1ec235dcd28e7c26571ceeb0e6b0cce991
ShubhamMahajan880 <mhajan.shubh@gmail.com>
2025-07-26 00:11:51 +0530

    LinkedList Implementations Start

--- testing.cpp
+++ testing.cpp
@@ -12,7 +12,50 @@
 // #include<iterator>
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+public:
+    Node *head;
+    Node *tail;
+
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    // Pushing from Front
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head = NULL)
+        {
+            head = tail = NULL;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+};
+
 int main()
 {
-
+    List ll;
+    ll.push_front(5);
 }
