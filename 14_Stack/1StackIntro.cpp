e1bd1bfa555537e8d9c7dc6f9ce81e93338edbae
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-07-30 14:11:41 +0530

    Stack Complete

--- 14_Stack/1StackIntro.cpp
+++ 14_Stack/1StackIntro.cpp
@@ -13,7 +13,7 @@
 // #include<stack>
 using namespace std;
 
-// 1) Stack Implementation using Vector -
+//1) Stack Implementation using Vector -
 //  class Stack
 //  {
 //      vector<T> vec;
@@ -256,7 +256,7 @@ So, Stack is -
 
 // ____________ ____________ ____________ ____________ ____________
 
-// Stack impmentation usig LL -
+//2) Stack impmentation usig LL -
 // Case 1 - using STL LL -
 
 // template <class T>
@@ -388,7 +388,7 @@ So, Stack is -
 // }
 // ____________ ____________ ____________ ____________ ____________
 
-// // Stack in STL -
+//3) Stack in STL -
 // int main()
 // {
 //     stack<int> s;
@@ -411,7 +411,8 @@ So, Stack is -
 //      */
 // }
 // ____________ ____________ ____________ ____________ ____________
-// Push at Bottom of the Stack -
+
+//4) Push at Bottom of the Stack -
 
 // void pushAtBottom(stack<int> &st, int val)
 // {
@@ -467,3 +468,428 @@ So, Stack is -
 //      */
 // }
 // ____________ ____________ ____________ ____________ ____________
+
+//5) Reverse a string using stack -
+// string reverseString(string str)
+// {
+//     string ans;
+//     stack<char> s;
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         s.push(str[i]);
+//     }
+
+//     while (!s.empty())
+//     {
+//         char top = s.top();
+//         ans += top;
+//         s.pop();
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     string str = "abcd";
+//     cout << "So, the entered string is - " << endl;
+//     for (auto i : str)
+//     {
+//         cout << i;
+//     }
+//     cout << endl;
+
+//     cout << "and the reverse string is - " << endl;
+//     cout << reverseString(str) << endl;
+//     /*
+//     So, the entered string is -
+//     abcd
+//     and the reverse string is -
+//     dcba
+// TC & SC = O(N) - as n elements , so n size stack and n size stack space
+//      */
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+//6) Reverse a  stack using recursion -
+
+// void pushAtBottom(stack<int> &s, int val) // - original stack me ki change na aaye isliye stack ko Reference& ke saath [ass krna jruri he]
+// {
+//     if (s.empty())
+//     {
+//         s.push(val);
+//         return;
+//     }
+
+//     int temp = s.top();
+//     s.pop();
+//     pushAtBottom(s, val);
+//     s.push(temp);
+// }
+
+// void reverseStack(stack<int> &s) // - original stack me ki change na aaye isliye stack ko Reference& ke saath [ass krna jruri he
+// {
+//     if (s.empty())
+//     {
+//         return;
+//     }
+
+//     int temp = s.top();
+//     s.pop();
+//     reverseStack(s);
+//     pushAtBottom(s, temp);
+// }
+
+// void printStack(stack<int> s)
+// {
+//     while (!s.empty())
+//     {
+//         cout << s.top() << " ";
+//         s.pop();
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     stack<int> s;
+//     s.push(4);
+//     s.push(3);
+//     s.push(2);
+//     s.push(1);
+
+//     cout << "Correct stack is - " << endl;
+//     printStack(s);
+
+//     reverseStack(s);
+//     cout << "& the reversed stack is - " << endl;
+//     printStack(s);
+//     /*
+//     Correct stack is -
+//     1 2 3 4
+//     & the reversed stack is -
+//     4 3 2 1
+
+//      */
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+//7) Stock Spain Problem -
+
+// void storckSpanProblem(vector<int> stock, vector<int> span)
+// {
+//     stack<int> s;
+//     s.push(0);
+//     span[0] = 1;
+
+//     for (int i = 1; i < stock.size(); i++)
+//     {
+//         int currPrice = stock[i];
+//         while (!s.empty() && currPrice >= stock[s.top()])
+//         {
+//             s.pop();
+//         }
+
+//         if (s.empty())
+//         {
+//             span[i] = i + 1;
+//         }
+//         else
+//         {
+//             int prevHigh = s.top();
+//             span[i] = i - prevHigh;
+//         }
+//     }
+//     for (int i = 0; i < span.size(); i++)
+//     {
+//         cout << span[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     vector<int> stock = {100, 80, 60, 70, 60, 85, 100};
+//     vector<int> span = {0, 0, 0, 0, 0, 0, 0};
+
+//     storckSpanProblem(stock, span);
+//     return 0;
+//     /*
+//     1 1 2 3 4 5 7
+//      */
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+//8) Next Greater Element  -
+
+// void nextGreaterElement(vector<int> arr, vector<int> ans)
+// {
+//     stack<int> s;
+//     int idx = arr.size() - 1;
+//     ans[idx] = -1;
+//     s.push(arr[idx]);
+
+//     for (idx = idx - 1; idx >= 0; idx--)
+//     {
+//         int curr = arr[idx];
+//         while (!s.empty() && curr >= s.top())
+//         {
+//             s.pop();
+//         }
+//         if (s.empty())
+//         {
+//             ans[idx] = -1;
+//         }
+//         else
+//         {
+//             ans[idx] = s.top();
+//         }
+//         s.push(curr);
+//     }
+
+//     for (int i = 0; i < ans.size(); i++)
+//     {
+//         cout << ans[i] << " ";
+//     }
+//     cout << endl;
+// }
+// int main()
+// {
+//     vector<int> arr = {6, 8, 0, 1, 3};
+//     vector<int> ans(6, 0);
+
+//     nextGreaterElement(arr, ans);
+// /*
+// 8 -1 1 3 -1 0
+//  */
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+//9) Valid Paranthesis Problem -
+
+// bool isValidParanthesis(string str)
+// {
+//     stack<char> st;
+
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         char ch = str[i];
+//         if (ch == '(' || ch == '{' || ch == '[') // opening condition
+//         {
+//             st.push(ch);
+//         }
+//         else // closing condiiton
+//         {
+//             if (st.empty()) // if nothing available insode stack
+//             {
+//                 /*
+//                 Enter the Paranthesis string for val 1  -
+//                 ({}}(){})
+//                 Enter the Paranthesis string for val 2  -
+//                 [{()}]
+//                 0
+//                 1
+
+//                 TC - O(n) - only time taken by string characters n
+//                 SC - O(n) - only space taken for n siz stack
+//                  */
+//                 return false;
+//             }
+
+//             // matching paranethiss conditiopn
+//             char top = st.top();
+//             if ((top == '(' && ch == ')') || (top == '{' && ch == '}') || (top == '[' && ch == ']'))
+//             {
+//                 st.pop();
+//             }
+//             else
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+
+//     if (st.empty()) // opening closing se match hone ke baad agr stack empty he to true otherwise false
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+
+// int main()
+// {
+//     string s1, s2;
+//     cout << "Enter the Paranthesis string for val 1  - " << endl;
+//     getline(cin, s1);
+
+//     cout << "Enter the Paranthesis string for val 2  - " << endl;
+//     getline(cin, s2);
+
+//     cout << isValidParanthesis(s1) << endl;
+//     cout << isValidParanthesis(s2) << endl;
+//     /*
+//     Enter the Paranthesis string for val 1  -
+//     ({}}(){})
+//     Enter the Paranthesis string for val 2  -
+//     [{()}]
+//     0
+//     1
+
+//     TC - O(n) - only time taken by string characters n
+//     SC - O(n) - only space taken for n siz stack
+//      */
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+//10) Duplicate  Paranthesis Problem -
+
+// bool isDuplicate(string str)
+// {
+//     stack<char> st;
+
+//     for (int i = 0; i < str.size(); i++)
+//     {
+//         char ch = str[i];
+//         if (ch != ')') // Non-CLosing
+
+//         {
+//             st.push(ch);
+//         }
+//         else // closing
+//         {
+//             if (st.top() == '(')
+//             {
+//                 return true; // Duplicate - nothing b/w opening & closing
+//             }
+
+//             while (st.top() != '(')
+//             {
+//                 st.pop();
+//             }
+//             st.pop();
+//         }
+//     }
+//     return false;
+// }
+
+// int main()
+// {
+//     string s1, s2;
+//     cout << "mention the expressions want to check for S1 - " << endl;
+//     getline(cin, s1);
+
+//     cout << "mention the expressions want to check for S2 - " << endl;
+//     getline(cin, s2);
+
+//     cout << isDuplicate(s1) << endl;
+//     cout << isDuplicate(s2) << endl;
+//     /*
+//     mention the expressions want to check for S1 -
+//     ((a+b))
+//     mention the expressions want to check for S2 -
+//     ((a+b)+(c+d))
+//     1
+//     0
+// TC & SC - O(N)
+//      */
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+//11) Max Area in the Histogram  -
+
+// void pritnArray(vector<int> height)
+// {
+//     for (int i = 0; i < height.size(); i++)
+//     {
+//         cout << height[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// void maxAreaHistogram(vector<int> height)
+// {
+//     int n = height.size();
+//     vector<int> nsl(n);
+//     vector<int> nsr(n);
+//     stack<int> s;
+
+//     // next smaller left
+//     nsl[0] = -1;
+//     s.push(0);
+//     for (int i = 0; i < height.size(); i++)
+//     {
+//         int curr = height[i];
+//         while (!s.empty() && curr <= height[s.top()])
+//         {
+//             s.pop();
+//         }
+
+//         if (s.empty())
+//         {
+//             nsl[i] = -1;
+//         }
+//         else
+//         {
+//             nsl[i] = s.top();
+//         }
+//         s.push(i);
+//     }
+//     while ((!s.empty()))
+//     {
+//         s.pop();
+//     }
+
+//     cout << " " << endl;
+//     pritnArray(nsl);
+
+//     // next smaller right
+//     s.push(n - 1);
+//     nsr[n - 1] = n;
+//     for (int i = n - 2; i >= 0; i--)
+//     {
+//         int curr = height[i];
+//         while (!s.empty() && curr <= height[s.top()])
+//         {
+//             s.pop();
+//         }
+//         if (s.empty())
+//         {
+//             nsr[i] = n; // although it should -1 but for managing the width will consider n
+//         }
+//         else
+//         {
+//             nsr[i] = s.top();
+//         }
+//         s.push(i);
+//     }
+//     cout << "" << endl;
+//     pritnArray(nsr);
+
+//     int maxArea = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int ht = height[i];
+//         int width = nsr[i] - nsl[i] - 1;
+//         int area = ht * width;
+
+//         maxArea = max(area, maxArea);
+//     }
+//     cout << "So, finally the max area of Histogram is - " << maxArea << endl;
+// }
+
+// int main()
+// {
+//     vector<int> height = {2, 1, 5, 6, 2, 3};
+//     maxAreaHistogram(height);
+//     /*
+
+//     -1 -1 1 2 1 4
+
+//     1 6 4 4 6 6
+//     So, finally the max area of Histogram is - 10
+
+//      */
+// }
+// ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________
