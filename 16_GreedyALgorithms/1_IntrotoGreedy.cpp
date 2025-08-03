cf6c61105512807ec0d75e7273565245298fb238
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-08-03 12:16:14 +0530

    GreedyAlgo Start

---
+++ 16_GreedyALgorithms/1_IntrotoGreedy.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include<algorithm>
+// #include<climits>
+// #include<string>
+// #include<cctype>
+// #include<vector>
+// #include<set>
+// #include<iomanip>
+// #include<cmath>
+// #include<list>
+// #include<iterator>
+// #include<stack>
+// #include <queue>
+// #include <deque>
+using namespace std;
+int main()
+{
+}
