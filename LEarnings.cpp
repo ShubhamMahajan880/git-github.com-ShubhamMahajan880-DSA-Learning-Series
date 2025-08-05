5533df7752e4c9ea87ea674879fc16ae8a7a8b77
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-08-05 16:51:58 +0530

    Tree Practice on Leetcode

--- LEarnings.cpp
+++ LEarnings.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <queue>
 using namespace std;
 
 class Node
@@ -8,64 +9,63 @@ public:
     Node *left;
     Node *right;
 
-    Node(int val)
+    Node(int data)
     {
-        data = val;
+        this->data = data;
         left = right = NULL;
     }
 };
 
 static int idx = -1;
-Node *treecreation(vector<int> nodes)
+Node *createtree(vector<int> nodes)
 {
     idx++;
     if (nodes[idx] == -1)
     {
         return NULL;
     }
+    Node *newnode = new Node(nodes[idx]);
+    newnode->left = createtree(nodes);
+    newnode->right = createtree(nodes);
 
-    Node *currnode = new Node(nodes[idx]);
-    currnode->left = treecreation(nodes);
-    currnode->right = treecreation(nodes);
-
-    return currnode;
+    return newnode;
 }
 
-void Preordertraversal(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    cout << root->data << " ";
-    Preordertraversal(root->left);
-    Preordertraversal(root->right);
-}
-
-void Inordertraversal(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    Inordertraversal(root->left);
-    cout << root->data << " ";
-    Inordertraversal(root->right);
-}
-
-void PostOrderTraversal(Node *root)
+void preOrderTraversal(Node *root)
 {
     if (root == NULL)
     {
         return;
     }
 
-    PostOrderTraversal(root->left);
-    PostOrderTraversal(root->right);
+    cout << root->data << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+void inOrderTraversal(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrderTraversal(root->left);
+    cout << root->data << " ";
+    inOrderTraversal(root->right);
+}
+
+void postOrderTraversal(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
     cout << root->data << " ";
 }
 
-void levelOrderTeaveresal(Node *root)
+void levelOrderTraversal(Node *root)
 {
     if (root == NULL)
     {
@@ -80,7 +80,6 @@ void levelOrderTeaveresal(Node *root)
         q.pop();
 
         cout << curr->data << " ";
-
         if (curr->left != NULL)
         {
             q.push(curr->left);
@@ -92,70 +91,75 @@ void levelOrderTeaveresal(Node *root)
     }
 }
 
-void anotherlevelordertraversal(Node *root)
+int heightoftree(Node *root)
 {
     if (root == NULL)
     {
-        return;
+        return 0;
     }
+    int leftheight = heightoftree(root->left);
+    int rightheight = heightoftree(root->right);
 
-    queue<Node *> q;
-    q.push(root);
-    q.push(NULL);
-    while (!q.empty())
-    {
-        Node *curr = q.front();
-        q.pop();
-
-        if (curr == NULL)
-        {
-            cout << endl;
-            if (q.empty())
-            {
-                break;
-            }
-            q.push(NULL);
-        }
-        else
-        {
-            cout << curr->data << " ";
-            if (curr->left != NULL)
-            {
-                q.push(curr->left);
-            }
-            if (curr->right != NULL)
-            {
-                q.push(curr->right);
-            }
-        }
-    }
+    int currheight = max(leftheight, rightheight) + 1;
+    return currheight;
 }
 
+int nodesintree(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int leftsidenodes = nodesintree(root->left);
+    int rightsidenodes = nodesintree(root->right);
+
+    int totalnodes = leftsidenodes + rightsidenodes + 1;
+    return totalnodes;
+}
+
+int sumofnodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int leftnodesum = sumofnodes(root->left);
+    int rightnodesum = sumofnodes(root->right);
+
+    int totalsumofnodes = leftnodesum + rightnodesum + root->data;
+    return totalsumofnodes;
+}
 int main()
 {
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-    Node *root = treecreation(nodes);
+    Node *root = createtree(nodes);
 
-    cout << "the Root of the Ndoe is - " << root->data << endl;
-    cout << "___________" << endl;
+    cout << "So, the basic root of the tree is - " << root->data << endl;
+    cout << "-------------" << endl;
 
-    cout << "Pre order Traversal for Elemenets is - " << endl;
-    Preordertraversal(root);
-    cout << "___________" << endl;
+    cout << "The Pre of Elements is - " << endl;
+    preOrderTraversal(root);
+    cout << "-------------" << endl;
 
-    cout << "TheInordeertraversal of elements are - " << endl;
-    Inordertraversal(root);
-    cout << "___________" << endl;
+    cout << "The IN of Elements is - " << endl;
+    inOrderTraversal(root);
+    cout << "-------------" << endl;
 
-    cout << "The PpostOrderTraveerdal of elements are - " << endl;
-    PostOrderTraversal(root);
-    cout << "___________" << endl;
+    cout << "The Post of Elements is - " << endl;
+    postOrderTraversal(root);
+    cout << "-------------" << endl;
 
-    cout << "Sorting after Level Oreder Trraversal is - " << endl;
-    levelOrderTeaveresal(root);
-    cout << "___________" << endl;
+    cout << "The Level Order  of Elements is - " << endl;
+    levelOrderTraversal(root);
+    cout << "-------------" << endl;
 
-    cout << "Using ANother Level Order Traversal the Elements of the Tree are given as - " << endl;
-    anotherlevelordertraversal(root);
-    cout << "___________" << endl;
+    cout << "hence the height of the tree is - " << heightoftree(root) << endl;
+    cout << "-------------" << endl;
+
+    cout << "So, the no of nodes of tree are - " << nodesintree(root) << endl;
+    cout << "-------------" << endl;
+
+    cout << "hence the total sum of all the nodes is - " << sumofnodes(root) << endl;
+    cout << "-------------" << endl;
+    return 0;
 }
--- testing.cpp
+++ testing.cpp
@@ -1,79 +1,21 @@
 #include <bits/stdc++.h>
+// #include <iostream>
+// #include<algorithm>
+// #include<climits>
+// #include<string>
+// #include<cctype>
+// #include<vector>
+// #include<set>
+// #include<iomanip>
+// #include<cmath>
+// #include<list>
+// #include<iterator>
+// #include<stack>
+// #include <queue>
+// #include <deque>
 using namespace std;
-
-class Node
-{
-public:
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val)
-    {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-static int idx = -1;
-Node *treecreation(vector<int> nodes)
-{
-    idx++;
-    if (nodes[idx] == -1)
-    {
-        return NULL;
-    }
-
-    Node *currnode = new Node(nodes[idx]);
-    currnode->left = treecreation(nodes);
-    currnode->right = treecreation(nodes);
-
-    return currnode;
-}
-
-void levelOrderTeaveresal(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    queue<Node *> q;
-    q.push(root);
-    q.push(NULL);
-    while (!q.empty())
-    {
-        Node *curr = q.front();
-        q.pop();
-
-        if (curr == NULL)
-        {
-            cout << endl;
-            if (q.empty())
-            {
-                break;
-            }
-            q.push(NULL);
-        }
-        else
-        {
-            cout << curr->data << " ";
-            if (curr->left != NULL)
-            {
-                q.push(curr->left);
-            }
-            if (curr->right != NULL)
-            {
-                q.push(curr->right);
-            }
-        }
-    }
-}
-
 int main()
 {
-    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-    Node *root = treecreation(nodes);
-    cout << "Sorting after Level Oreder Trraversal is - " << endl;
-    levelOrderTeaveresal(root);
-    cout << "___________" << endl;
-}
+    
+}   
+
