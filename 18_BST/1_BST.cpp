ad1c0fdbf7fe886abc2aa364e196cc989d1f1164
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-08-12 01:01:57 +0530

    Added missing local files from old repo

--- 18_BST/1_BST.cpp
+++ 18_BST/1_BST.cpp
@@ -739,6 +739,212 @@ using namespace std;
 
 // ____________ ____________ ____________ ____________ ____________
 
-// 9) Size of Largest BSt in BT - 
+// 9) Size of Largest BSt in BT -
 
+// class Node
+// {
+// public:
+//     int data;
+//     Node *left;
+//     Node *right;
 
+//     Node(int data)
+//     {
+//         this->data = data;
+//         left = right = NULL;
+//     }
+// };
+
+// class Info
+// {
+// public:
+//     bool isBST;
+//     int min;
+//     int max;
+//     int sz;
+
+//     Info(bool isBST, int min, int max, int sz)
+//     {
+//         this->isBST = isBST;
+//         this->min = min;
+//         this->max = max;
+//         this->sz = sz;
+//     }
+// };
+
+// int maxSize = 0;
+
+// Info *largestBST(Node *root)
+// {
+//     if (root == NULL)
+//     {
+//         return new Info(true, INT_MAX, INT_MIN, 0);
+//     }
+//     // OR another base case
+//     /* if (root == NULL)
+//     {
+//         return NULL;
+//     }
+//     if (root->left == NULL && root->right)
+//     {
+//         new Info(true, root->data, root->data, 1);
+//     }
+//  */
+
+//     Info *leftInfo = largestBST(root->left);
+//     Info *rightInfo = largestBST(root->right);
+
+//     int currMin = min(root->data, min(leftInfo->min, rightInfo->min));
+//     int currMax = max(root->data, max(leftInfo->max, rightInfo->max));
+//     int currSz = leftInfo->sz + rightInfo->sz + 1;
+
+//     if (leftInfo->isBST && rightInfo->isBST &&
+//         root->data > leftInfo->max && root->data < rightInfo->min)
+//     {
+//         maxSize = max(maxSize, currSz);
+//         return new Info(true, currMin, currMax, currSz);
+//     }
+
+//     return new Info(false, currMin, currMax, currSz);
+// }
+
+// int main()
+// {
+//     Node *root = new Node(50);
+//     root->left = new Node(30);
+//     root->left->left = new Node(5);
+//     root->left->right = new Node(20);
+
+//     root->right = new Node(60);
+//     root->right->left = new Node(45);
+//     root->right->right = new Node(70);
+//     root->right->right->left = new Node(65);
+//     root->right->right->right = new Node(80);
+
+//     largestBST(root);
+//     cout << "max size: " << maxSize << endl;
+//     return 0;
+//     /*
+//     max size: 5
+
+//     */
+// }
+
+// ____________ ____________ ____________ ____________ ____________
+
+// 9.1) Largest Root of BSt  in BT -
+
+// ____________ ____________ ____________ ____________ ____________
+
+// 10) Merge  2 BST's -
+
+// class Node
+// {
+// public:
+//     int data;
+//     Node *left, *right;
+//     Node(int data)
+//     {
+//         this->data = data;
+//         left = right = NULL;
+//     }
+// };
+
+// // Inorder traversal to store elements
+// void getInorder(Node *root, vector<int> &nodes)
+// {
+//     if (root == NULL)
+//         return;
+//     getInorder(root->left, nodes);
+//     nodes.push_back(root->data);
+//     getInorder(root->right, nodes);
+// }
+
+// // Convert sorted vector to balanced BST
+// Node *BSTfromSortedVec(vector<int> &nodes, int st, int end)
+// {
+//     if (st > end)
+//         return NULL;
+//     int mid = st + (end - st) / 2;
+//     Node *curr = new Node(nodes[mid]);
+//     curr->left = BSTfromSortedVec(nodes, st, mid - 1);
+//     curr->right = BSTfromSortedVec(nodes, mid + 1, end);
+//     return curr;
+// }
+
+// // Merge two BSTs
+// Node *mergeBST(Node *root1, Node *root2)
+// {
+//     vector<int> nodes1, nodes2, merged;
+//     getInorder(root1, nodes1);
+//     getInorder(root2, nodes2);
+
+//     // Merge sorted arrays
+//     int i = 0, j = 0;
+//     while (i < nodes1.size() && j < nodes2.size())
+//     {
+//         if (nodes1[i] < nodes2[j])
+//             merged.push_back(nodes1[i++]);
+//         else
+//             merged.push_back(nodes2[j++]);
+//     }
+//     while (i < nodes1.size())
+//         merged.push_back(nodes1[i++]);
+//     while (j < nodes2.size())
+//         merged.push_back(nodes2[j++]);
+
+//     // Build balanced BST from merged array
+//     return BSTfromSortedVec(merged, 0, merged.size() - 1);
+// }
+
+// // Print inorder
+// void inorderPrint(Node *root)
+// {
+//     if (!root)
+//         return;
+//     inorderPrint(root->left);
+//     cout << root->data << " ";
+//     inorderPrint(root->right);
+// }
+
+// void preorderPrint(Node *root)
+// {
+//     if (!root)
+//         return;
+//     cout << root->data << " ";
+//     preorderPrint(root->left);
+//     preorderPrint(root->right);
+// }
+
+// int main()
+// {
+//     // Your given BSTs
+//     Node *root1 = new Node(2);
+//     root1->left = new Node(1);
+//     root1->right = new Node(4);
+
+//     Node *root2 = new Node(9);
+//     root2->left = new Node(3);
+//     root2->right = new Node(12);
+
+//     // Merge and print
+//     Node *mergedRoot = mergeBST(root1, root2);
+
+//     cout << "Inorder of merged BST: ";
+//     inorderPrint(mergedRoot);
+//     cout << "\n";
+
+//     cout << "Preorder of merged BST: ";
+//     preorderPrint(mergedRoot);
+//     cout << "\n";
+
+//     return 0;
+//     /*
+
+// Inorder of merged BST: 1 2 3 4 9 12
+// Preorder of merged BST: 3 1 2 9 4 12
+
+//     */
+// }
+
+// ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________ ____________
