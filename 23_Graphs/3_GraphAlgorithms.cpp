aca0b932a3c3f78802a9179f03a05f81e73a6036
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-09-11 00:29:57 +0530

    Graph ALmost Completed

--- 23_Graphs/3_GraphAlgorithms.cpp
+++ 23_Graphs/3_GraphAlgorithms.cpp
@@ -95,61 +95,198 @@ Why Bellman Ford Algo for V-1 times - Because we're ensuring that the longest pa
 For V nodes there will be V-1 Edges.
  */
 
-class Edge
+// class Edge
+// {
+// public:
+//     int v;
+//     int wt;
+
+//     Edge(int v, int wt)
+//     {
+//         this->v = v;
+//         this->wt = wt;
+//     }
+// };
+
+// void bellmanFord(vector<vector<Edge>> graph, int V, int src) // O(V.E)
+// {
+//     vector<int> dist(V, INT_MAX);
+//     dist[src] = 0;
+
+//     for (int i = 0; i < V - 1; i++) // V
+//     {
+//         for (int u = 0; u < V; u++) // E
+//         {
+//             for (Edge e : graph[u])
+//             {
+//                 if (dist[e.v] > dist[u] + e.wt)
+//                 {
+//                     dist[e.v] = dist[u] + e.wt;
+//                 }
+//             }
+//         }
+//     }
+//     for (int i = 0; i < V; i++)
+//     {
+//         cout << dist[i] << " ";
+//     }
+//     cout << endl;
+// }
+// int main()
+// {
+//     int V = 6;
+//     vector<vector<Edge>> graph(V);
+
+//     graph[0].push_back(Edge(1, 2)); // (destintn,weight)
+//     graph[0].push_back(Edge(2, 4));
+
+//     graph[1].push_back(Edge(2, 1));
+//     graph[1].push_back(Edge(3, 7));
+
+//     graph[2].push_back(Edge(4, 3));
+
+//     graph[3].push_back(Edge(5, 1));
+
+//     graph[4].push_back(Edge(3, 2));
+//     graph[4].push_back(Edge(2, 5));
+
+//     bellmanFord(graph, V, 0); // 0 2 3 8 6 9
+// }
+
+// ____________ ____________ ____________ ____________ ____________
+
+// 12) Minimum SPanning Tree(MST) -
+/*
+- Spanning Tree is a Tree derived from Graph which are must Connected Graph and includes All the vertex without forming the cycle.
+- No. of edges in Spanning Tree = No. of Vertex -1
+- Minimum Spannig Tree is  for Undirected Weighted Grapph only.
+- A spanning Tree with Minimum weight  is MST.
+- For getting MST from Graph there is 2 Algorithms - Prim's Algom & Kruskal's Algom.
+ */
+
+//  12.1) Prim's Algorithm -
+
+// class Graph
+// {
+//     int V;
+//     list<pair<int, int>> *l; // int, int : neighbour + wt
+//     bool isUndir;
+
+// public:
+//     Graph(int V, bool isUndir = true)
+//     {
+//         this->V = V;
+//         l = new list<pair<int, int>>[V];
+//         this->isUndir = isUndir;
+//     }
+
+//     void addEdge(int u, int v, int wt) // u-->v
+//     {
+//         l[u].push_back(make_pair(v, wt));
+//         if (isUndir)
+//         {
+//             l[v].push_back(make_pair(u, wt));
+//         }
+//     }
+
+//     void printGraph()
+//     {
+//         for (int u = 0; u < V; u++)
+//         {
+//             cout << u << " : ";
+//             for (auto nbr : l[u])
+//             {
+//                 cout << "(" << nbr.first << ", " << nbr.second << ") ";
+//             }
+//             cout << endl;
+//         }
+//     }
+
+//     void primsAlgo(int src)
+//     {
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//         // (wt,u) -> minHeap
+//         vector<bool> mst(V, false);
+
+//         pq.push(make_pair(0, src));
+//         int ans = 0;
+
+//         while (pq.size() > 0)
+//         {
+//             int u = pq.top().second;
+//             int wt = pq.top().first;
+//             pq.pop();
+
+//             if (!mst[u])
+//             {
+//                 mst[u] = true;
+//                 ans += wt;
+//                 list<pair<int, int>> neighbour = l[u];
+//                 for (pair<int, int> n : neighbour)
+//                 {
+//                     int v = n.first;
+//                     int currWt = n.second;
+//                     pq.push(make_pair(currWt, v));
+//                 }
+//             }
+//         }
+//         cout << "final cost of MST = " << ans << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Graph graph(4);
+
+//     graph.addEdge(0, 1, 10);
+//     graph.addEdge(0, 2, 15);
+//     graph.addEdge(0, 3, 30);
+
+//     graph.addEdge(1, 3, 40);
+
+//     graph.addEdge(2, 3, 50);
+
+//     graph.primsAlgo(0); // final cost of MST = 55
+// }
+// ____________ ____________ ____________ ____________ ____________
+
+// 12.2) - Leetcode 1584 - Min Cost to Connect All Points
+
+class Solution
 {
 public:
-    int v;
-    int wt;
-
-    Edge(int v, int wt)
+    int MinCostConnectPoints(vector<vector<int>> &points)
     {
-        this->v = v;
-        this->wt = wt;
-    }
-};
+        int V = points.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        // (wt,u)
 
-void bellmanFord(vector<vector<Edge>> graph, int V, int src) // O(V.E)
-{
-    vector<int> dist(V, INT_MAX);
-    dist[src] = 0;
+        int src = 0;
+        vector<bool> mst(V, false);
+        pq.push(make_pair(0, src));
+        int minCOst = 0;
 
-    for (int i = 0; i < V - 1; i++) // V
-    {
-        for (int u = 0; u < V; u++) // E
+        while (pq.size() > 0)
         {
-            for (Edge e : graph[u])
+            int u = pq.top().second;
+            int cost = pq.top().first;
+            pq.pop();
+
+            if (!mst[u])
             {
-                if (dist[e.v] > dist[u] + e.wt)
+                mst[u] = true;
+                minCOst += cost;
+
+                for (int v = 0; v < V; v++)
                 {
-                    dist[e.v] = dist[u] + e.wt;
+                    if (u != v) // u--v
+                    {
+                        int wt = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                        pq.push(make_pair(wt, v));
+                    }
                 }
             }
         }
+        return minCOst;
     }
-    for (int i = 0; i < V; i++)
-    {
-        cout << dist[i] << " ";
-    }
-    cout << endl;
-}
-int main()
-{
-    int V = 6;
-    vector<vector<Edge>> graph(V);
-
-    graph[0].push_back(Edge(1, 2)); // (destintn,weight)
-    graph[0].push_back(Edge(2, 4));
-
-    graph[1].push_back(Edge(2, 1));
-    graph[1].push_back(Edge(3, 7));
-
-    graph[2].push_back(Edge(4, 3));
-
-    graph[3].push_back(Edge(5, 1));
-
-    graph[4].push_back(Edge(3, 2));
-    graph[4].push_back(Edge(2, 5));
-
-    bellmanFord(graph, V, 0);//0 2 3 8 6 9 
-
-}
+};
