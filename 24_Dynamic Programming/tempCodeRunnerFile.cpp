899d15570f5f01f8ded6887097e66837eaa7ce08
Shubham Mahajan <mhajan.shubh@gmail.com>
2025-09-18 17:21:57 +0530

    DP about to end

--- 24_Dynamic Programming/tempCodeRunnerFile.cpp
+++ 24_Dynamic Programming/tempCodeRunnerFile.cpp
@@ -1,38 +1,56 @@
-int getMinDiff(vector<int> nums)
+int matrixChainMultiplicationTabulation(vector<int> arr) // O(n^3)
 {
-    int totalSum = 0;
-    for (int el : nums)
+  int n = arr.size();
+  vector<vector<int>> dp(n, vector<int>(n, 0));
+
+  // dp[i][i] = 0 (only one matrix, no multiplication)
+  for (int i = 1; i < n; i++)
+  {
+    dp[i][i] = 0;
+  }
+
+  // bottom up fill
+  for (int len = 2; len < n; len++) // length of chain
+  {
+    for (int i = 1; i <= n - len; i++)
     {
-        totalSum += el;
+      int j = i + len - 1;
+      dp[i][j] = INT_MAX;
+
+      for (int k = i; k < j; k++)
+      {
+        int cost1 = dp[i][k];
+        int cost2 = dp[k + 1][j];
+        int currCost = cost1 + cost2 + (arr[i - 1] * arr[k] * arr[j]);
+        dp[i][j] = min(dp[i][j], currCost);
+      }
     }
+  }
 
-    int n = nums.size();
-    int W = totalSum / 2;
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
-
-    for (int i = 1; i < n + 1; i++)
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
     {
-        for (int j = 1; j < W + 1; j++)
-        {
-            if (nums[i - 1] <= j)
-            {
-                dp[i][j] = max(nums[i - 1] + dp[i - 1][j - nums[i - 1]], dp[i - 1][j]);
-            }
-            else
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
+      cout << dp[i][j] << " ";
     }
+    cout << endl;
+  }
 
-    int grp1sum = dp[n][W];
-    int grp2sum = totalSum - grp1sum;
-    return abs(grp1sum - grp2sum);
+  return dp[1][n - 1];
 }
 
 int main()
 {
-    vector<int> nums = {1, 6, 11, 5};
-    cout << getMinDiff(nums) << endl;
-    return 0;
+  vector<int> arr = {1, 2, 3, 4, 3}; // n-> n-1 matrices (1 to n-1)
+
+  cout << matrixChainMultiplicationTabulation(arr) << endl;
+  /*
+  0 0 0 0 0
+  0 0 6 18 30
+  0 0 0 24 48
+  0 0 0 0 36
+  0 0 0 0 0
+
+  30
+  */
 }
